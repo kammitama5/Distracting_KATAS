c055826c00de30ab23f90e9b3b17462d373889cd
kammitama5 <krystal.maughan@gmail.com>
2016-11-22 15:17:39 -0800

    chops off first letter and last letter of any string

---
+++ Kata_distraction_001.py
@@ -0,0 +1,16 @@
+def remove_char(s):
+    
+    t = s[:1] #- s[1:]
+    w = s[1:]
+    z = s[1:-1]
+    print (z)
+    
+    return t
+
+  
+remove_char("Tests")
+remove_char('eloquent')
+remove_char('country')
+remove_char('person')
+remove_char('place')
+remove_char('ok')
