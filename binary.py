14fe42b5971ad42c082eb82808aa968f048625bd
kammitama5 <krystal.maughan@gmail.com>
2016-12-05 21:39:43 -0800

    um..I named a variable "watabe"

--- binary.py
+++ binary.py
@@ -2,6 +2,7 @@ def add_binary(a,b):
     binary = bin(a + b)
     watabe = str(binary)
     print watabe[2:]
+    # I am aware that it isn't good practice to name variables watabe. Sorry.
     
     
 add_binary(1, 1)
