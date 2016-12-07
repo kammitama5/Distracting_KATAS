f3c92095422dff90063801255c3036c3fc2b5f2d
kammitama5 <krystal.maughan@gmail.com>
2016-12-07 12:40:05 -0800

    listfilter kata

---
+++ listfilter.py
@@ -0,0 +1,14 @@
+def filter_list(l):
+  mynewlist = []
+  anotherlist = []
+  for i in l:
+    try:
+      int(i)
+      if type((i)) != str:
+        (mynewlist.append(i))
+     
+    except ValueError:
+        if i.isdigit():
+          anotherlist.append(i)
+          
+  return mynewlist
