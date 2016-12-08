a8a939d97781a8d15ca87878dfeed9d4b486dee1
kammitama5 <krystal.maughan@gmail.com>
2016-12-08 10:27:08 -0800

    solution for this kata "highest profit wins"

---
+++ highestprofit.py
@@ -0,0 +1,19 @@
+def min_max(lst):
+
+    
+    list1 = []
+    for i in lst:
+      max1 = (max(lst))
+      min1 = (min(lst))
+    list3 = min1,max1
+    list4 = list(list3)
+    
+    
+  
+    print list4 
+
+
+
+min_max([1,2,3,4,5]) # returns [1,5]
+min_max([2334454,5]) # returns [5, 2334454]
+
