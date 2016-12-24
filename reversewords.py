2a61b4f8abc8445db0d2e909cf4d8c4f5c83da40
kammitama5 <krystal.maughan@gmail.com>
2016-12-23 21:39:22 -0800

    solved another-> reverse w0rds

---
+++ reversewords.py
@@ -0,0 +1,16 @@
+def reverse_words(str):
+  str1 = str.split(" ")
+  #print str1
+  arr = []
+  for i in str1:
+     arr.append(i[::-1])
+  #print arr
+  arr1 = " ".join(arr)
+  return arr1
+  
+  
+  
+  
+
+
+reverse_words('This is an example!') #'sihT si na !elpmaxe'
