7bfa26ca3cabdf3411ff63b41c8a8625d8bfe68f
kammitama5 <krystal.maughan@gmail.com>
2016-12-26 15:57:17 -0800

    solved

---
+++ findmaximumandminimumvaluesofalist.py
@@ -0,0 +1,17 @@
+def min(arr):
+     #print arr
+     for i in range (1, len(arr)):
+       if arr[i] < arr[0]:
+         arr[0] = arr[i]
+     return arr[0]
+      
+     
+    
+
+def max(arr):
+    #print arr
+    
+    for i in range (1, len(arr)):
+       if arr[i] > arr[0]:
+         arr[0] = arr[i]
+    return arr[0]
