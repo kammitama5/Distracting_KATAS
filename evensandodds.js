47b21c60e90f9693983298a6cc33b68ae2c322ea
kammitama5 <krystal.maughan@gmail.com>
2016-12-26 20:54:39 -0800

    Add files via upload

---
+++ evensandodds.js
@@ -0,0 +1,12 @@
+function evensAndOdds(num){
+	if (num % 2 == 0) {
+    console.log(num.toString(2))
+  }
+  else
+  {
+    console.log(num.toString(16))
+  }
+}
+
+evensAndOdds(2)
+evensAndOdds(13)
