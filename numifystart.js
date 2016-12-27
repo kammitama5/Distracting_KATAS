ddb9dfc592b8fc6a6a76a8599929b7a6b34c0031
kammitama5 <krystal.maughan@gmail.com>
2016-12-27 07:47:44 -0800

    Add files via upload

---
+++ numifystart.js
@@ -0,0 +1,34 @@
+function Word2Num(str) {
+    counter = 0
+    if str == "zero"{
+      counter = counter + 0
+      }
+    else if str == "one"{
+      counter = counter + 1
+    }
+    else if str == "two"{
+      counter = counter + 2
+    }
+    else if str == "three"{
+      counter = counter + 3
+    }
+    else if str == "four"{
+      counter = counter + 4
+    }
+    else if str == "five"{
+      counter = counter + 5
+    }
+    else if str == "six"{
+      counter = counter + 6
+    }
+    else if str == "seven"{
+      counter = counter + 7
+    }
+    else if str == "eight"{
+      counter = counter + 8
+    }
+    else if str == "nine"{
+      counter = counter + 9
+    }
+    return counter
+}
