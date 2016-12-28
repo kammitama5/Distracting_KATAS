4088fa253af50e26495eb30d077073e959a5fd5e
kammitama5 <krystal.maughan@gmail.com>
2016-12-28 12:40:10 -0800

    works

---
+++ lengthofthelinesegment.py
@@ -0,0 +1,8 @@
+def length_of_line(array):
+    w1, w2 = zip(*array)
+    #print w1, w2
+    side1  = (w2[1] - w2[0]) * (w2[1] - w2[0])
+    side2 = (w1[1] - w1[0]) * (w1[1] - w1[0])
+    hyp = (side1 + side2) ** 0.50
+    
+    return str("%.2f" % hyp)
