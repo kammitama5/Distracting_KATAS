1b57c3b4840437c9384058bfcf9f4337ad897b3f
kammitama5 <krystal.maughan@gmail.com>
2016-12-28 10:35:23 -0800

    Add files via upload

---
+++ determineoffspreingsexbasedongenesXXandXYchromosomes.py
@@ -0,0 +1,5 @@
+def chromosome_check(sperm):
+    if sperm == "XX":
+        return 'Congratulations! You\'re going to have a daughter.'
+    else:
+        return 'Congratulations! You\'re going to have a son.'
