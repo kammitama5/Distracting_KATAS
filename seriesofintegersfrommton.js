736fb74f8408ac255dfdf37788af770cf57c19a4
kammitama5 <krystal.maughan@gmail.com>
2016-12-27 18:05:29 -0800

    Add files via upload

---
+++ seriesofintegersfrommton.js
@@ -0,0 +1,16 @@
+function generateIntegers(m, n){
+    arr = []
+  
+    for (i = m; i <= n; i++)
+    {
+      
+      arr.push(i)
+    }
+    console.log(arr)
+
+    
+}
+    
+    
+    
+generateIntegers(2, 5)  //, [2, 3, 4, 5]
