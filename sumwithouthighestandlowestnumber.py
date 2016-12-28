146868cb50cfd7d95d1c208eb53a5546737849d0
kammitama5 <krystal.maughan@gmail.com>
2016-12-28 11:18:41 -0800

    solved it!

---
+++ sumwithouthighestandlowestnumber.py
@@ -0,0 +1,19 @@
+def sum_array(arr):
+  
+  if (arr == None):
+    return 0
+  
+  elif (len(arr) <= 1):
+    return 0
+  
+    
+  else:
+    w = sorted(arr)
+    del w[-1:]
+    del w[0]
+    counter = 0 
+    for i in w:
+      counter = counter + i 
+    
+    
+    return counter
