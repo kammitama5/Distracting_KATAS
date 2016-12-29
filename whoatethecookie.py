5919172cdfb56e6e83ae98d6d0aa1d857da32b58
kammitama5 <krystal.maughan@gmail.com>
2016-12-29 13:00:31 -0800

    works

---
+++ whoatethecookie.py
@@ -0,0 +1,12 @@
+def cookie(x):
+    if type(x) == int:
+        return "Who ate the last cookie? It was Monica!"
+    elif type(x) == float:
+        return "Who ate the last cookie? It was Monica!"
+    elif type(x) == str:
+        return "Who ate the last cookie? It was Zach!"
+    else:
+        return "Who ate the last cookie? It was the dog!"
+		
+		
+		
