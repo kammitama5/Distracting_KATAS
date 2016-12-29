c7e956af9f6f02df66e901e07e5c526a244deec7
kammitama5 <krystal.maughan@gmail.com>
2016-12-29 13:31:26 -0800

    easy

---
+++ stopworrying.js
@@ -0,0 +1,23 @@
+function worries(str){
+    if (str == "Making last-minute changes"){
+      return "Stop worrying, everything will be all right!"
+    }
+    else if (str == 'Sitting in a dark room and shaking'){
+      return 'Stop worrying, everything will be all right!'
+    }
+    else if (str == 'Crying'){
+      return 'Stop worrying, everything will be all right!'
+    }
+    else if (str == 'Laughing hysterically'){
+      return 'Stop worrying, everything will be all right!'
+    }
+    else if (str == 'Not eating'){
+      return 'Stop worrying, everything will be all right!'
+    }
+    else{
+      return 'Do you really care?'
+    }
+
+
+}
+
