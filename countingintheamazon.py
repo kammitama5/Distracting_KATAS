15a111249f63c36d6804dd34148230d2c1358caa
kammitama5 <krystal.maughan@gmail.com>
2016-12-28 18:49:08 -0800

    works

---
+++ countingintheamazon.py
@@ -0,0 +1,11 @@
+def count_arara(n):
+  if (n == 1):
+    return "anane"
+  elif (n == 2):
+    return "adak"
+  elif (n % 2) == 0:
+     
+    return ("adak " * (n / 2)).rstrip()
+  else:
+      q = ("adak " * ((n - 1) / 2) + "anane")
+      return q.rstrip()
