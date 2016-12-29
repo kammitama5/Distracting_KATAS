7e6692d986635db78bf8f49c1a95fd19f5344449
kammitama5 <krystal.maughan@gmail.com>
2016-12-29 00:54:13 -0800

    Update fixyourcodebeforethegardendies.py

--- fixyourcodebeforethegardendies.py
+++ fixyourcodebeforethegardendies.py
@@ -12,3 +12,4 @@ rain_amount(40) # "Your plant has had more than enough water for today!")
 rain_amount(39) # "You need to give your plant 1mm of water")
 rain_amount(5) #"You need to give your plant 35mm of water")
 rain_amount(0) #"You need to give your plant 40mm of water")
+
