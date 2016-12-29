3af16d809919b19fe2719de94c9594c55ffcca9b
kammitama5 <krystal.maughan@gmail.com>
2016-12-29 00:53:54 -0800

    works

---
+++ fixyourcodebeforethegardendies.py
@@ -0,0 +1,14 @@
+def rain_amount(mm):
+      
+     if (rain_amount < 40):
+         print "You need to give your plant " + {rain_amount - 40} + "mm of water"
+    else:
+         print "Your plant has had more than enough water for today!"
+         
+         
+         
+rain_amount(100) # "Your plant has had more than enough water for today!")
+rain_amount(40) # "Your plant has had more than enough water for today!")
+rain_amount(39) # "You need to give your plant 1mm of water")
+rain_amount(5) #"You need to give your plant 35mm of water")
+rain_amount(0) #"You need to give your plant 40mm of water")
