a735456d4d6c6c11a51a3bcd8e77490ae1164545
kammitama5 <krystal.maughan@gmail.com>
2016-12-29 21:59:51 -0800

    easy

---
+++ billgatespercentagecalculator.js
@@ -0,0 +1,9 @@
+
+function gatesCalculator(num1, num2) {
+  var percent = (num1 / num2) * 100.0
+  var percent1 = percent.toString()
+  /* Calculate what percentage of "num2" is "num1." */
+  return "%" + percent1;
+
+  
+  
