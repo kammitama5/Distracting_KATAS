0f0f61bb5f681bb25d7c09d636f7505fc61be9f8
kammitama5 <krystal.maughan@gmail.com>
2016-12-31 04:33:44 -0800

    woomp

---
+++ squarensum.py
@@ -0,0 +1,15 @@
+def square_sum(numbers):
+    counter = 0
+    for i in numbers:
+         
+        #print (i**2)
+        counter = counter + (i **2)
+    return counter
+    
+        
+    
+    
+    
+square_sum([1,2]) # 'squareSum did not return a value')
+square_sum([1,2]) # 5)
+square_sum([0, 3, 4, 5]) # 50)
