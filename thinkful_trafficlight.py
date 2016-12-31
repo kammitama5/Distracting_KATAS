ef246e63fd50f03f353f50a065aab20f652dfb26
kammitama5 <krystal.maughan@gmail.com>
2016-12-30 22:12:41 -0800

    easy

---
+++ thinkful_trafficlight.py
@@ -0,0 +1,11 @@
+def update_light(current):
+    if current == "green":
+        return "yellow"
+    elif current == "yellow":
+        return "red"
+    elif current == "red":
+        return "green"
+    else:
+		return False
+		
+		
