421c0d15667092d1f80a8659080cd1db403b2925
kammitama5 <krystal.maughan@gmail.com>
2016-12-31 19:57:43 -0800

    works

---
+++ areyouplayingbanjo.py
@@ -0,0 +1,8 @@
+def areYouPlayingBanjo(name):
+    # Implement me!
+    if name[0] == 'r' or name[0] == 'R':
+        return "{} plays banjo".format(name)
+    else:
+        return "{} does not play banjo".format(name)
+		
+		
