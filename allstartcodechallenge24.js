89b13bd01caa3f673e1ba0f18aaaaf8d8abb5893
kammitama5 <krystal.maughan@gmail.com>
2017-01-02 13:08:16 -0800

    works

---
+++ allstartcodechallenge24.js
@@ -0,0 +1,12 @@
+function hypotenuse(a, b){
+   var hyp = (Math.sqrt((a * a) + (b * b)))
+   return hyp
+    
+}
+
+function leg(c, a){
+  var side = (Math.sqrt((c * c) - (a * a)))
+  return side
+   
+}
+
