4e849c66dd3be2e04f1cc26541e2351c264179fb
kammitama5 <krystal.maughan@gmail.com>
2017-01-01 23:39:43 -0800

    hahahah

---
+++ freudiantranslator.py
@@ -0,0 +1,12 @@
+def to_freud(sentence):
+    a = sentence.split()
+    b = len(a)
+    return (b * "sex ").rstrip()
+  
+    return
+  
+  
+  
+to_freud("Heya Krystal")
+to_freud("How are you?")
+
