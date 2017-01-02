8bc44c63e6325a154f48886dc87de07d9bd8b68e
kammitama5 <krystal.maughan@gmail.com>
2017-01-02 12:53:10 -0800

    works

---
+++ allstartchallenge27.js
@@ -0,0 +1,16 @@
+function factorial(x){
+    arr = []
+    for (var i = 1; i <= x; i++){
+      
+        y = arr.push(i);
+    }
+    fact = 1 
+    for (var j = 1; j < arr.length + 1; j++){
+        fact = fact * j
+        
+      
+    }
+    return(fact)
+  
+}
+
