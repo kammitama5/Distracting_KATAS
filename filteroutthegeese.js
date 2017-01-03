2ab4d4872433b314135a7ce85e15cf3ae6fe4cf5
kammitama5 <krystal.maughan@gmail.com>
2017-01-02 23:02:40 -0800

    works

---
+++ filteroutthegeese.js
@@ -0,0 +1,7 @@
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  
+
+  return arr = birds.filter(function(x) { return geese.indexOf(x) < 0 })
+};
+
