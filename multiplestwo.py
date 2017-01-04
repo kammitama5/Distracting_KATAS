f5d00707b09ba707d1c3c8748c7095752f1116ed
kammitama5 <krystal.maughan@gmail.com>
2017-01-04 01:33:04 -0800

    works

---
+++ multiplestwo.py
@@ -0,0 +1,11 @@
+def multiples(x):
+    if (x % 49 == 0) and (x % 3 == 0):
+        return "Fang"
+    elif (x % 7 == 0):
+        return "Fizz"
+    elif (x % 15 == 0):
+        return "Foo"
+    else:
+        return "Far"
+		
+		
