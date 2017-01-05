c46e8a2629c98d92b1df91204ddd99d610c32490
kammitama5 <krystal.maughan@gmail.com>
2017-01-04 19:21:06 -0800

    works

---
+++ Exclamationmarksseries11.replaceallvoweltoexclamationmarkinthesentence.py
@@ -0,0 +1,16 @@
+def replace_exclamation(s):
+  
+      
+      
+      return s.replace('A', '!').replace('E', '!').replace('I', '!').replace('O','!').replace('U','!').replace('a','!').replace('e','!').replace('i','!').replace('o','!').replace('u','!')
+  
+       
+      
+      
+      
+      
+replace_exclamation("Hi!") # "H!!")
+replace_exclamation("!Hi! Hi!") # "!H!! H!!")
+replace_exclamation("aeiou") # "!!!!!")
+replace_exclamation("ABCDE") # "!BCD!")
+
