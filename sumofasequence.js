afdedb195ad4064316b64184525d64125d9d4be3
kammitama5 <krystal.maughan@gmail.com>
2017-01-06 11:13:18 -0800

    works

---
+++ sumofasequence.js
@@ -0,0 +1,13 @@
+function sequenceSum(begin, end, step){
+
+      
+      counter = 0
+      
+      for (var i = begin; i <= end; i+=step){
+        
+          counter = counter + i
+      }
+      return counter
+
+}
+
