eae0b3464b6e49a049c4b77a2eeeb84fd02a13d8
kammitama5 <krystal.maughan@gmail.com>
2017-01-06 19:51:56 -0800

    ugh. this may be so wrong -> buggy

---
+++ buggy_lettercheck.js
@@ -0,0 +1,27 @@
+function letterCheck(arr){
+      a = arr[0]
+      b = arr[1]
+      
+      arr1 = []
+      for (var i = 0; i < a.length; i++){
+          arr1.push(a[i])
+      }
+      a1 = arr1.sort()
+      
+      
+      arr2 = []
+      for (var j = 0; j < b.length; j++){
+          arr2.push(b[j])
+      }
+      a2 = arr2.sort()
+      
+      console.log(a1)
+      console.log(a2)
+      
+      
+      
+
+  
+  
+      return 
+}
