c6f13950ab83182721c391ef69eee88cd7972178
kammitama5 <krystal.maughan@gmail.com>
2017-01-07 11:39:36 -0800

    works

---
+++ simplebeadsdcount.py
@@ -0,0 +1,15 @@
+function sequenceSum(begin, end, step){
+
+      
+      counter = 0
+      
+      for (var i = begin; i <= end; i+=step){
+        
+          counter = counter + i
+      }
+      return counter
+
+}
+
+
+
