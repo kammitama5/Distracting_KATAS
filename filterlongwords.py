719df6660d58b53cc5290ebefd29bedba43e2f87
kammitama5 <krystal.maughan@gmail.com>
2017-01-09 14:24:32 -0800

    works

---
+++ filterlongwords.py
@@ -0,0 +1,12 @@
+def filter_long_words(sentence, n):
+      arr = []
+      a = sentence.split()
+      for i in a:
+      
+        if len(i) > n:
+          c = i 
+          d = arr.append(c)
+      return arr 
+	  
+	  
+	  
