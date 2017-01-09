cf23a9f7257f89cdc42342c7e34ef83a31206f5a
kammitama5 <krystal.maughan@gmail.com>
2017-01-08 20:11:41 -0800

    works

---
+++ evensorodds_.js
@@ -0,0 +1,11 @@
+function evensAndOdds(num){
+	    if (num % 2 == 0){
+	      a = Number(num).toString(2)
+	      return a
+	    }
+	    else{
+	      b = (Number(num).toString(16))
+	      return b
+	    }
+}
+
