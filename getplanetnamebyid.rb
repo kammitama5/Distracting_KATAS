c8452d5dedbaccad958f69b6e71a5068c7c9dc87
kammitama5 <krystal.maughan@gmail.com>
2017-01-09 05:29:12 -0800

    woop woop ruby!

---
+++ getplanetnamebyid.rb
@@ -0,0 +1,25 @@
+def get_planet_name(id)
+  # This doesn't work; Fix it!
+  name = ''
+  case id
+    when 1
+      name = "Mercury"
+    when 2
+      name = "Venus"
+    when 3
+      name = "Earth"
+    when 4
+      name = "Mars"
+    when 5
+      name = "Jupiter"
+    when 6
+      name = "Saturn"
+    when 7
+      name = "Uranus"  
+    when 8
+      name = "Neptune"
+    else
+      name = "blah"
+  end
+  return name
+end
