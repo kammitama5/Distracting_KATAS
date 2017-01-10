8ca3b8b96783ff21101daac92ceb874ebe61174b
kammitama5 <krystal.maughan@gmail.com>
2017-01-09 17:19:34 -0800

    debug works

---
+++ debugbasiccalculator.py
@@ -0,0 +1,16 @@
+def calculate(a, o, b):
+    result = 0
+     
+    if(o == "+"):
+        return a + b
+    elif(o == "-"):
+        return a - b
+    elif(o == "/" and b != 0):
+        return a / b
+    elif(o == "*"):
+        return a * b
+    else:
+        return None
+    return result
+	
+	
