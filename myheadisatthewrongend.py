b745718dd347ac9675a26422495daf77d05b8a1c
kammitama5 <krystal.maughan@gmail.com>
2017-01-12 08:53:35 -0800

    works!

---
+++ myheadisatthewrongend.py
@@ -0,0 +1,14 @@
+def fix_the_meerkat(arr):
+    arr1 = []
+  
+    a = arr[-1]
+    b = arr[1]  
+    c = arr[0]
+    
+    arr1.append(a)
+    arr1.append(b)
+    arr1.append(c)
+    
+    return arr1
+	
+	
