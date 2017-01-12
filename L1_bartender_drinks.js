12e705859b96c4af78064d3a26489e1651e515bc
kammitama5 <krystal.maughan@gmail.com>
2017-01-12 15:57:38 -0800

    works

---
+++ L1_bartender_drinks.js
@@ -0,0 +1,27 @@
+function getDrinkByProfession(param){
+  
+      a = ((param).toLowerCase())
+      if (a == "jabroni"){
+          return "Patron Tequila"
+      }
+      else if(a == "school counselor"){
+          return "Anything with Alcohol"
+      }
+      else if(a == "programmer"){
+          return "Hipster Craft Beer"
+      }
+      else if(a == "bike gang member"){
+          return "Moonshine"
+      }
+      else if(a == "politician"){
+          return "Your tax dollars"
+      }
+      else if(a == "rapper"){
+          return "Cristal"
+      }
+      else{
+          return "Beer"
+      }
+      
+
+}
