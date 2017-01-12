c043fdec8fad49ec0505f77382a8e3dfc4f182f0
kammitama5 <krystal.maughan@gmail.com>
2017-01-12 03:53:22 -0800

    solved!

--- Fizz_Buzz.py
+++ Fizz_Buzz.py
@@ -1,16 +1,32 @@
-def fizzbuzz(n):    
-    arr = []
-    for n in range(1, n+1):
-      if ((n % 5 == 0) and (n % 3 == 0)):
-          a = arr.append("FizzBuzz")
-        
-      elif (n % 5 == 0):
-          a = arr.append("Buzz")
-      elif (n % 3 == 0):
-          a = arr.append("Fizz")
-      else:
-          a = arr.append(n)
-        
-    return arr
-	
-	
+def solution(number):
+  count = 0 # 15
+  count1 = 0 # 3
+  count2 = 0 # 5
+  count3 = 0 # not used
+  arr = []
+  
+  for i in range(0, number):
+  
+    if (i % 15 == 0):
+      count = count + 1
+    elif (i % 3 == 0):
+      count1 = count1 + 1 
+      b = count1 
+      #print b
+    elif (i % 5 == 0):
+      count2 = count2  + 1
+      #print c
+    else:
+      count3 = count3 + 1
+      d = count3
+  a =  count1 
+  b =  count2
+  c =  count -1
+
+  arr.append(a)
+  arr.append(b)
+  arr.append(c)
+
+  return arr
+  
+  
