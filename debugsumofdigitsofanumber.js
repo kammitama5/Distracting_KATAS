9d62ac41a4714ae13d5b6a90e5a8caef2351284b
kammitama5 <krystal.maughan@gmail.com>
2017-01-13 11:50:12 -0800

    solved

--- debugsumofdigitsofanumber.js
+++ debugsumofdigitsofanumber.js
@@ -1,14 +1,9 @@
-
 function getSumOfDigits(integer) {
-  var sum = 0
-  var digits = Math.abs(integer)
-  var digits1 = digits.toString()
-  
-  for (var i = 0; i < digits1.length; i++){
-      sum += parseInt(digits1.charAt(i), 10)
-      
+  var sum = 0;
+  var digits =  Math.abs(integer).toString();
+  console.log(digits)
+  for(var i = 0; i < digits.length; i++) {
+    sum = sum + parseInt(digits[i]);
   }
- 
-  return sum
+  return sum;
 }
-
