fcfeef47c8802a549ae5a73926d12d781ad42672
kammitama5 <krystal.maughan@gmail.com>
2017-01-16 13:36:47 -0800

    solved!

---
+++ sayhello!.js
@@ -0,0 +1,12 @@
+function greet(name) {
+      if (name == ""){
+        return null
+      }
+      else if (name == null){
+        return null
+      }
+      else{
+        return "hello " + name + '!'
+      }
+}
+
