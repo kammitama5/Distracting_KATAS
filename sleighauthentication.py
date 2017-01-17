36ddfaceffd1bdd17d1db3a18d335da487f51599
kammitama5 <krystal.maughan@gmail.com>
2017-01-17 15:30:53 -0800

    solved

---
+++ sleighauthentication.py
@@ -0,0 +1,7 @@
+class Sleigh(object):
+    def authenticate(self, name, password):
+        if ((name == 'Santa Claus') and (password == 'Ho Ho Ho!')):
+            return True
+        else:
+            return False
+        
