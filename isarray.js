9b331e2d6f96059537026798e332e0838ac48734
kammitama5 <krystal.maughan@gmail.com>
2017-01-25 14:44:56 -0600

    solved

---
+++ isarray.js
@@ -0,0 +1,10 @@
+function isArray(value) {
+      if (Object.prototype.toString.call(value) === '[object Array]'){
+        return (true)
+      }
+      else{
+        return (false)
+      }
+      
+}
+
