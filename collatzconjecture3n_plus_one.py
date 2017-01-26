adc1c8351054faab73019f9e33ca92bb809e6bb9
kammitama5 <krystal.maughan@gmail.com>
2017-01-25 21:36:11 -0600

    solved..two for one

---
+++ collatzconjecture3n_plus_one.py
@@ -0,0 +1,12 @@
+def hotpo(n):
+    arr = []
+    while n != 1:
+        if n % 2 == 0:
+            n = int(n / 2)
+            arr.append(n)
+        else:
+            n = int(3 * n + 1)
+            arr.append(n)
+    else:
+       return len(arr)
+        
