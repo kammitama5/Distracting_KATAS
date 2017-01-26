37a9a229ef3b8e4a542703d469efb6fbeaa4ce8b
kammitama5 <krystal.maughan@gmail.com>
2017-01-25 21:49:53 -0600

    solved

---
+++ KISS.py
@@ -0,0 +1,12 @@
+def is_kiss(words):
+        a = words.split(" ")
+        arr = []
+        b = len(a)
+        for i in a:
+          c = arr.append(len(i))
+        x = max(arr)
+        if b >= x:
+          return "Good work Joe!"
+        else:
+          return "Keep It Simple Stupid"
+		  
