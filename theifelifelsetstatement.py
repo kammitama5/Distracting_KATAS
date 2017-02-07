bf7135295d81c036d4e259ed7ea661c95f9bd010
kammitama5 <krystal.maughan@gmail.com>
2017-02-07 14:07:48 -0800

    solved -> easyyyy

---
+++ theifelifelsetstatement.py
@@ -0,0 +1,12 @@
+def checkDiscount(cost):
+    if (cost >= 450):
+      total = cost - 100
+      return total
+    elif (cost >= 300):
+      total = cost - 50
+      return total
+    else:
+      total = cost 
+      return total
+	  
+	  
