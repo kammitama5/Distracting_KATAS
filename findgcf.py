82b71efeda4b923088facebe55f2bb1b90694467
kammitama5 <krystal.maughan@gmail.com>
2017-02-07 11:10:26 -0800

    works but not allowing to pass tests :(

---
+++ findgcf.py
@@ -0,0 +1,18 @@
+def largest_factor(num1, num2):
+    arr1 = []
+    arr2 = []
+    
+    
+    for i in range(1, num1+1):
+      if num1 % i == 0:
+        arr1.append(i)
+    for i in range(1, num2+1):
+      if num2 % i == 0:
+        arr2.append(i)
+
+    
+    c = list(set(arr1) & set(arr2))
+    c1 = c[-1]
+    
+    return c1
+	
