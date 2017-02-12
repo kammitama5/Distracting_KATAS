d7c7b0b5d082983dc521fa5cf209c52fa3018dbc
kammitama5 <krystal.maughan@gmail.com>
2017-02-11 20:37:10 -0800

    works but not in format needed

---
+++ pattern.js
@@ -0,0 +1,13 @@
+function pattern(n){
+  if (n < 1){
+    console.log("")
+  }
+  else{
+    for (var i = 1; i <= n; i++){
+    a = i.toString()
+    console.log(a.repeat(i))}
+    }
+  
+  return
+}
+
