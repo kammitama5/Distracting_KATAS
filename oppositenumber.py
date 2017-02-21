5ce4612fa6fe2193fcea63ee3b0ab4275ed01d3d
kammitama5 <krystal.maughan@gmail.com>
2017-02-21 13:40:34 -0800

    easy peasy

--- oppositenumber.py
+++ oppositenumber.py
@@ -1,9 +1,6 @@
 def opposite(number):
-  if number < 0:
-    return abs(number)
-  elif number > 0:
-    return (0 - number)
+  if (number < 0):
+      return abs(number)
   else:
-    return 0
-    
-    
+      return 0 - number
+	  
