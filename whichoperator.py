43e52511083feda0233338386252d95aa6497c8d
kammitama5 <krystal.maughan@gmail.com>
2017-03-01 12:08:35 -0800

    solved

---
+++ whichoperator.py
@@ -0,0 +1,10 @@
+def whichOper(num1, num2, oper):
+    if oper == "add":
+        return num1 + num2
+    elif oper == "subtract":
+        return num1 - num2
+    elif oper == "multiply":
+        return num1 * num2
+    elif oper == "divide":
+        return num1 / num2
+		
