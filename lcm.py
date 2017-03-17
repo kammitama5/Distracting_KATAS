c107567dd79f97cd13d7d1fe3624ddca6c8c2ff0
kammitama5 <krystal.maughan@gmail.com>
2017-03-17 13:41:38 -0700

    solved

---
+++ lcm.py
@@ -0,0 +1,15 @@
+def lcm(a, b):
+  if a > b:
+    c = a 
+  else:
+    c = b 
+    
+  while(True):
+    if ((c % a == 0) and (c % b == 0)):
+      lcm = c 
+      break;
+    c += 1
+
+  return lcm
+  
+  
