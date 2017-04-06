42d2e7301c48952cc8c2dc5c2b79c8ef114e268e
kammitama5 <krystal.maughan@gmail.com>
2017-04-05 20:02:02 -0700

    i cheated and used if else instead of switch -> welp!

---
+++ switchonsomepizza.js
@@ -0,0 +1,13 @@
+function switchOnPizza(exp) {
+  var answer = "";
+  // Only change code below this line
+   if (exp == "hungry and thirsty"){
+     return "Let's get pizza and drinks.";
+   }
+   else if (exp == "hungry"){
+     return "Let's get pizza.";
+   }
+   else if (exp == "thirsty"){
+     return "Let's get drinks.";
+   }
+}
