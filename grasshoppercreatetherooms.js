f00828e3ba19080cf16b74070a0c49263cb850ad
kammitama5 <krystal.maughan@gmail.com>
2017-04-20 15:33:06 -0700

    this was weird

---
+++ grasshoppercreatetherooms.js
@@ -0,0 +1,6 @@
+// Add rooms here
+var rooms = {
+      room1 : {name: "room1", description: "black", completed: "done"},
+      room2 : {name: "room2", description: "white", completed: "done"},
+      room3 : {name: "room3", description: "blue", completed: "done"}
+};
