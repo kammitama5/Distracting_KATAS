112244ebfa1562a1c7a8658c126caefa32427098
kammitama5 <krystal.maughan@gmail.com>
2017-05-12 14:40:36 -0700

    solved

---
+++ abugstriologyletmath.randomdecideyourfuture.js
@@ -0,0 +1,16 @@
+function yourFutureCareer(career){
+	  var career = Math.random();
+		if (career <= 0.32) {
+			return "FrontEnd Developer"
+      }
+		else if (career <= 0.65) {
+			return "BackEnd Developer"
+      }
+		else {
+			return 'Full-Stack Developer'
+		}
+    }
+
+
+yourFutureCareer();
+
