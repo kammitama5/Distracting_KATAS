68061ce45da820e66548b14ab36f9a169e5f527d
kammitama5 <krystal.maughan@gmail.com>
2017-07-09 16:11:45 -0700

    Add files via upload

---
+++ signin_signout_butsignit.js
@@ -0,0 +1,16 @@
+function sign(number){
+  if (isNaN(number) == true){
+    return(NaN)
+  }
+  else if (number == 0){
+    return(0)
+  }
+  else if (number < 0){
+    return(-1)
+  }
+  else{
+    return(1)
+  }
+ 
+  return 
+}
