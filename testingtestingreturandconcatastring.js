192d35d74cc0d568b48208a25e9f1fed96d58600
kammitama5 <krystal.maughan@gmail.com>
2017-07-25 09:00:18 -0700

    Add files via upload

---
+++ testingtestingreturandconcatastring.js
@@ -0,0 +1,8 @@
+function getRes(myScore) {
+    if (myScore >= 70){
+      return 'Congratulations, you have passed! Thank you for taking part.'
+    } 
+    else{
+      return 'Sorry, you have failed. Better luck next time! Thank you for taking part.'
+    }
+}
