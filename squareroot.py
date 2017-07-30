a47cc911bbe3c893742586080a82e64398d59f94
kammitama5 <krystal.maughan@gmail.com>
2017-07-29 21:21:17 -0700

    Add files via upload

---
+++ squareroot.py
@@ -0,0 +1,15 @@
+def square_root_me(sqrt):
+    a = sqrt 
+    arr = []
+    for i in range(1, a):
+      if (i * i == a):
+        arr.append(i)
+        break;
+    
+    return arr[0]
+  
+  
+square_root_me(2704)#, 52)
+square_root_me(1681)#, 41)
+square_root_me(5476)#, 74)
+
