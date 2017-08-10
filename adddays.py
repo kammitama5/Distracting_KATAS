c19bc3a88894862be62a0560db7f3e433b968eab
vdalv <vdalv@protonmail.com>
2017-08-09 21:04:47 -0700

    Cleans up the adddays file

--- adddays.py
+++ adddays.py
@@ -1,15 +1,11 @@
-
 def nextDay(year, month, day):
-    
-    if (month == 12) and (day == 30):
+    if (month == 12) and (day == 31):
         day = 1
         month = 1
-        year = year + 1
+        year += 1
     elif (day == 30):
         day = 1
-        month = month + 1
+        month += 1
     else:
-        
-        day = day + 1
+        day += 1
     return(year, month, day)
-
