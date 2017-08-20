4fc2c0b216437ecbe3da92dfad0ad1317e3df18b
kammitama5 <krystal.maughan@gmail.com>
2017-08-20 12:22:25 -0700

    Add files via upload

---
+++ regexkataignoringwhitespaceandbigletters.js
@@ -0,0 +1,11 @@
+/*YOUR IMPLENTATIONB*/
+function returnMatch(searchTerm, subject) {
+  var subject1 = subject.replace(/ /g,'')
+  var subject2 = subject1.toLowerCase()
+  if (subject2.search(searchTerm) != -1){
+    return 'test f'
+  }
+  else{
+    return 'test f'
+  }
+}
