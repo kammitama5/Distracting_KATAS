c3dcc6a50c79ba07b49a74d0ea2b5f6ac6579f51
kammitama5 <krystal.maughan@gmail.com>
2017-10-10 15:18:31 -0700

    Add files via upload

---
+++ astrangetriptothemarket.js
@@ -0,0 +1,14 @@
+function isLockNessMonster(s) {
+    var a = 'tree fiddy'
+    var b = '3.50'
+    var c = 'three fifty'
+    
+    if (s.includes(a) || s.includes(b) || s.includes(c)){
+      return true
+    }
+      else{
+        return false
+      }
+    return
+}
+
