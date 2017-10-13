53d742535e5861f0513322717b28dcf4e01b355c
kammitama5 <krystal.maughan@gmail.com>
2017-10-13 11:56:49 -0700

    Add files via upload

--- heronsformula.py
+++ heronsformula.py
@@ -1,9 +1,6 @@
-import math
 def heron(a, b, c):
-      s = (a + b + c) / 2 
-      a1 = s - a 
-      b1 = s - b 
-      c1 = s - c
-      A = math.sqrt(s * a1 * b1 * c1)
-      return int(A)
-	  
+    import math
+    s = (a + b + c) / 2 
+    area = (s * ((s - a) * (s - b) * (s - c)))
+    return math.sqrt(area)
+	
