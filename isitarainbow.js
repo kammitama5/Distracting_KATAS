f203022efb3d3cfaa1a68e7cd43ba1191124012f
kammitama5 <krystal.maughan@gmail.com>
2017-10-18 08:14:24 -0700

    Add files via upload

---
+++ isitarainbow.js
@@ -0,0 +1,11 @@
+function sortRainbow(colour){
+    
+    if ((colour[0]==('red')) && (colour[1]==('orange')) && (colour[2]==('yellow')) && (colour[3]==('green')) && (colour[4]==('blue')) && (colour[5]==('indigo')) && (colour[6]==('violet'))){
+      return true
+    }
+    else{
+      return false
+    }
+    return
+}
+
