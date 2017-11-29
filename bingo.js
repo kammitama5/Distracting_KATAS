38c9f24239abd1194e251ed297f4ff1100ba5c21
kammitama5 <krystal.maughan@gmail.com>
2017-11-29 09:22:18 -0800

    Add files via upload

---
+++ bingo.js
@@ -0,0 +1,13 @@
+function bingo (arr) {
+  //There was a farmer who had a dog...
+  if ((arr.includes(2)) && (arr.includes(9)) && (arr.includes(14)) && (arr.includes(7)) && (arr.includes(15)))
+  {
+    return("WIN")
+  }
+  else
+  {
+    return("LOSE")
+  }
+  return
+}
+
