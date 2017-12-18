5364ac64a2cba27fa0ca7b7079021acb478cbf81
kammitama5 <krystal.maughan@gmail.com>
2017-12-18 08:00:52 -0800

    Add files via upload

---
+++ defineacardsuit.js
@@ -0,0 +1,19 @@
+function defineSuit(card) {
+  if (card[card.length-1] == '♣')
+  {
+  return 'clubs'
+  }
+  else if (card[card.length-1] == '♠')
+  {
+  return 'spades'
+  }
+  else if (card[card.length-1] == '♦')
+  {
+  return 'diamonds'
+  }
+  else if (card[card.length-1] == '♥')
+  {
+  return 'hearts'
+  }
+}
+
